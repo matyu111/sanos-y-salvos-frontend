b4b91d093568918400618f21d2572f8107548295
Juan Vargas <jua.vargasc@duocuc.cl>
2026-05-08 23:54:50 -0400

    feature: implemento login funcional con jwt

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,14 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import Register from '../pages/Register';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default function AppRouter() {
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+
+function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<h1>Dashboard</h1>} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default AppRouter;                
